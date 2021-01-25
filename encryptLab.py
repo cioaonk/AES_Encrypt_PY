11dfdf4f6c1135af0a726db3425ef1dd82307d59
cioaonk <42707320+cioaonk@users.noreply.github.com>
2021-01-25 02:10:45 -0700

    Update encryptLab.py

--- encryptLab.py
+++ encryptLab.py
@@ -2,7 +2,7 @@ import os
 import sys
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
-key = b"This is encrypted by me Ian Cook"
+key = b"*#*INSERT KEY HERE*#*"
 iv = os.urandom(16)
 inputFile = sys.stdin.buffer.read()
 cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
